3873c5aabc444791f3a044b568af0cf2edbd7059
Matthew Mateusz <mateuszm@matthewm.com.pl>
2019-12-02 20:33:01 -0600

    Autonomous Work:  - Added cases left, middle, and right.  - Phase A of autonomous completed     - Going from the wall to the skystone, picking it up, and moving backwards a little bit.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutonomous.java
@@ -10,11 +10,39 @@ public class TestAutonomous extends FunctionLibrary {
     initVuforia();
     initTfod();
     tfod.activate();
-
+    setupHardware();
     waitForStart();
 
+    vertical();
     encoderDriveDistance(speed_half, 1 * one_tile, 2);
-    testBlockThing(500, 2);
+    //skyStone location = testBlockThing(500, 2);
+    skyStone location = skyStone.right;
+    RotateArm(false, 2.5);
+
+    if (location.equals(skyStone.left)) {
+        encoderDriveDistance(speed_half, 0.3 * one_tile, 2);
+        setGrabber(grabber.left, grabState.close);
+        encoderDriveDistance(speed_half, -0.3 * one_tile, 2);
+        RotateArm(true, 2.5);
+
+    } else if (location.equals(skyStone.middle)) {
+        encoderDriveDistance(speed_half, 0.3 * one_tile, 2);
+        setGrabber(grabber.right, grabState.close);
+        encoderDriveDistance(speed_half, -0.3 * one_tile, 2);
+        RotateArm(true, 2.5);
+
+    } else if (location.equals(skyStone.right)) {
+        horizontal();
+        encoderDriveDistance(speed_half, -0.25 * one_tile, 2);
+        vertical();
+        encoderDriveDistance(speed_half, 0.3 * one_tile, 2);
+        setGrabber(grabber.right, grabState.close);
+        encoderDriveDistance(speed_half, -0.3 * one_tile, 2);
+        RotateArm(true, 2.5);
+
+    } else {
+
+    }
 
     }
 }
