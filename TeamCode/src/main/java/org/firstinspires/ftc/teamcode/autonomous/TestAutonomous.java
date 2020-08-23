9dd5ba4242f6720de1c79a7c9a748979faa3c783
Matthew <mateuszm@matthewm.com.pl>
2020-08-22 19:45:44 -0500

    Simple movement function.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Test Autonomous", group="INDEV")
+public class TestAutonomous extends Automation {
+
+    public void instruction() {
+
+    }
+}
