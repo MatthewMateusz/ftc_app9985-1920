d68ebb8c8264c94a7ae53cef7c5cf5e7c5327e2e
Matthew <mateuszm@matthewm.com.pl>
2020-08-22 20:19:26 -0500

    Implemented test concurrent actions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.java
@@ -5,7 +5,22 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name="Test Autonomous", group="INDEV")
 public class TestAutonomous extends Automation {
 
-    public void instruction() {
+    Thread moveServo = new Thread(new ToTop());
 
+    public void instruction() throws InterruptedException {
+
+        moveServo.start();
+        //encoderDrive(10000, 10000, 10000, 10000, 0.1, 30, true);
+        moveServo.join();
+
+    }
+
+    private class ToTop implements Runnable {
+
+        @Override
+        public void run() {
+            runToTop();
+            runToRear();
+        }
     }
 }
