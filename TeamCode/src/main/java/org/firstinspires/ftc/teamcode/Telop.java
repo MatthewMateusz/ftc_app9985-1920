a0574cbfada802bd10fcb35536ecadaec1f8cc24
NickKisch <nisckkisch3@gmail.com>
2020-09-02 19:24:37 -0500

    Simplified teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telop.java
@@ -14,8 +14,7 @@ public class Telop extends OpMode {
     /* Declare OpMode members. */
     Hardware robot       = new Hardware(); // use the class created to define a Pushbot's hardware
 
-    double          servoOffsetH  = 0.0;                  // Servo mid position
-    double          servoOffsetV  = 0.5;
+
     //final double    servoSpeedH   = 0.004;
     //final double    servoSpeedV   = 0.004;
 
@@ -53,70 +52,94 @@ public class Telop extends OpMode {
 
     @Override
     public void loop() {
-
         double leftFrontSpeed;
         double rightFrontSpeed;
         double leftRearSpeed;
         double rightRearSpeed;
 
-
-
-
-
-        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-        if (gamepad1.left_stick_y < 0) {
-            leftFrontSpeed = (gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
-            rightFrontSpeed = (-gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
-            leftRearSpeed = (gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
-            rightRearSpeed = (-gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
-
-            //robot.servo_frontLeft.setPosition(.5);
-            //robot.servo_frontRight.setPosition(.5);
-            //robot.servo_rearRight.setPosition(.5);
-            //robot.servo_rearLeft.setPosition(.5);
-        }
-        else if (gamepad1.left_stick_y > 0) {
-            leftFrontSpeed = (gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
-            rightFrontSpeed = (-gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
-            leftRearSpeed = (gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
-            rightRearSpeed = (-gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
-
-            //robot.servo_frontLeft.setPosition(.5);
-            //robot.servo_frontRight.setPosition(.5);
-            //robot.servo_rearRight.setPosition(.5);
-            //robot.servo_rearLeft.setPosition(.5);
-        }
-        else if (gamepad1.left_stick_x > 0) {
-            leftFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-            rightFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-            leftRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-            rightRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-
-        }
-
-        else if (gamepad1.left_stick_x < 0) {
-            leftFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-            rightFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-            leftRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-            rightRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
-
-        }
-        else {
-            leftFrontSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
-            rightFrontSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
-            leftRearSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
-            rightRearSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
-
-            //robot.servo_frontLeft.setPosition(1);
-            //robot.servo_frontRight.setPosition(0);
-            //robot.servo_rearRight.setPosition(0);
-            //robot.servo_rearLeft.setPosition(1);
-        }
+//add this vairable.  "CC" stands for controller coefficient and is used with mecanum wheels so the x and y sticks don't add up to more than 1 for a pair of motors (this would be bad since the motor value is limited to 1.  The motors moving in the opposing direction would not be limited
+        double CC = .5;
 
         if (gamepad1.y)
             robot.servo_left_cont.setPower(.2);
         else
             robot.servo_left_cont.setPower(0);
+
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+
+//This code is for mecanum wheels mounted out the front and rear of robot (team 9985)
+        leftFrontSpeed = (gamepad1.left_stick_y*(CC) - gamepad1.left_stick_x*(CC) + gamepad1.left_trigger*(CC) - gamepad1.right_trigger*(CC));
+        rightFrontSpeed = (-gamepad1.left_stick_y*(CC) - gamepad1.left_stick_x*(CC) + gamepad1.left_trigger*(CC) - gamepad1.right_trigger*(CC));
+        leftRearSpeed = (gamepad1.left_stick_y*(CC) + gamepad1.left_stick_x*(CC) + gamepad1.left_trigger*(CC) - gamepad1.right_trigger*(CC));
+        rightRearSpeed = (-gamepad1.left_stick_y*(CC) +gamepad1.left_stick_x*(CC) + gamepad1.left_trigger*(CC) - gamepad1.right_trigger*(CC));
+
+        robot.motor_frontLeft.setPower(leftFrontSpeed);
+        robot.motor_frontRight.setPower(rightFrontSpeed);
+        robot.motor_rearLeft.setPower(leftRearSpeed);
+        robot.motor_rearRight.setPower(rightRearSpeed);
+
+        telemetry.addData("LeftFront:",leftFrontSpeed);
+        telemetry.addData("RightFront:",rightFrontSpeed);
+        telemetry.addData("LeftRear:",leftRearSpeed);
+        telemetry.addData("RightRear:",rightRearSpeed);
+
+        telemetry.update();
+//This code is for mecanum wheels mounted out the sides of robot (team 11283)
+
+
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        //if (gamepad1.left_stick_y < 0) {
+           // leftFrontSpeed = (gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightFrontSpeed = (-gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+            //leftRearSpeed = (gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+            //rightRearSpeed = (-gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+
+            //robot.servo_frontLeft.setPosition(.5);
+            //robot.servo_frontRight.setPosition(.5);
+            //robot.servo_rearRight.setPosition(.5);
+            //robot.servo_rearLeft.setPosition(.5);
+        }
+        //else if (gamepad1.left_stick_y > 0) {
+            //leftFrontSpeed = (gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightFrontSpeed = (-gamepad1.left_stick_y + gamepad1.left_trigger - gamepad1.right_trigger);
+            //leftRearSpeed = (gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+            //rightRearSpeed = (-gamepad1.left_stick_y - gamepad1.left_trigger + gamepad1.right_trigger);
+
+            //robot.servo_frontLeft.setPosition(.5);
+            //robot.servo_frontRight.setPosition(.5);
+            //robot.servo_rearRight.setPosition(.5);
+            //robot.servo_rearLeft.setPosition(.5);
+        //}
+        //else if (gamepad1.left_stick_x > 0) {
+            //leftFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+            //leftRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+
+        //}
+
+        //else if (gamepad1.left_stick_x < 0) {
+          //  leftFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightFrontSpeed = (-gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+            //leftRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightRearSpeed = (gamepad1.left_stick_x + gamepad1.left_trigger - gamepad1.right_trigger);
+
+        //}
+        //else {
+            //leftFrontSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightFrontSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
+            //leftRearSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
+            //rightRearSpeed = (gamepad1.left_trigger - gamepad1.right_trigger);
+
+            //robot.servo_frontLeft.setPosition(1);
+            //robot.servo_frontRight.setPosition(0);
+            //robot.servo_rearRight.setPosition(0);
+            //robot.servo_rearLeft.setPosition(1);
+        //}
+
+
 
 
 
@@ -130,15 +153,15 @@ public class Telop extends OpMode {
         //}
         //else {
 
-        robot.motor_frontLeft.setPower(leftFrontSpeed);
-        robot.motor_frontRight.setPower(rightFrontSpeed);
-        robot.motor_rearLeft.setPower(leftRearSpeed);
-        robot.motor_rearRight.setPower(rightRearSpeed);
+        //robot.motor_frontLeft.setPower(leftFrontSpeed);
+        //robot.motor_frontRight.setPower(rightFrontSpeed);
+        //robot.motor_rearLeft.setPower(leftRearSpeed);
+        //robot.motor_rearRight.setPower(rightRearSpeed);
 
-        //robot.servo_frontLeft.setPosition(-.5*gamepad1.right_stick_x+.5);
-        //robot.servo_rearRight.setPosition(-.5*gamepad1.right_stick_x+.5);
-        //robot.servo_rearLeft.setPosition(-.5*gamepad1.right_stick_x+.5);
-        //robot.servo_frontRight.setPosition(-.5*gamepad1.right_stick_x+.5);
+        /*robot.servo_frontLeft.setPosition(-.5*gamepad1.right_stick_x+.5);
+        robot.servo_rearRight.setPosition(-.5*gamepad1.right_stick_x+.5);
+        robot.servo_rearLeft.setPosition(-.5*gamepad1.right_stick_x+.5);
+        robot.servo_frontRight.setPosition(-.5*gamepad1.right_stick_x+.5);*/
 
                 //Moves front servos
         //if (gamepad2.dpad_left)
@@ -163,9 +186,9 @@ public class Telop extends OpMode {
         //telemetry.addData("Servo Offset V","Offset V = %.2f", servoOffsetV);
 
         //telemetry.addData("left",  "%.2f", left);
-        telemetry.update();
+        //telemetry.update();
 
-    }
+    //}
 
     /*
      * Code to run ONCE after the driver hits STOP
